1ae5241a20cf4a1ca279e5c5b8eaa3c89e70b95a
Siameseg <psyhakhom@gmail.com>
2017-01-25 12:35:49 -0500

    changed route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get  'static_pages/help'
-  get  'static_pages/home'
+  root  'static_pages#home'
 
 end
